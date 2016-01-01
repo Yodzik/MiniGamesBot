a40567f241974d65ff9dcf60c6e8b2447e5a9f16
Ja <misiek777a@interia.pl>
2016-01-01 02:42:20 +0100

    Added comments

--- MGB_one/MGBModel/Settings.cs
+++ MGB_one/MGBModel/Settings.cs
@@ -6,16 +6,21 @@ using System.Windows.Forms;
 
 namespace MGB_one
 {
+    //Settings for entire program
     public static class Settings
     {
+        //number of levels in minigames
         public const int MGB_MAX_GAME_LVL = 5;
+        //number of max points for minigames in one sesion per account
         public const byte MGB_MAX_GAME_POINTS = 20;
+        //file path for list of BaseAccounts
         public static string BaseAccountsFilePath;
 
         static Settings()
         {
-            //wczytywanie ustawien z xml
+            //default BaseAccounts file path
             Settings.BaseAccountsFilePath = Application.StartupPath + @"\BaseAccounts.xml";
+            //todo - loading settings from xml file
         }
     }
 }
