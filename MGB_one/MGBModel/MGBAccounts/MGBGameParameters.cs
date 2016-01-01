baaf9a77b7eede5b00b3857cb49e4e4cc109470c
Ja <misiek777a@interia.pl>
2016-01-01 01:05:30 +0100

    Added comments

--- MGB_one/MGBModel/MGBAccounts/MGBGameParameters.cs
+++ MGB_one/MGBModel/MGBAccounts/MGBGameParameters.cs
@@ -5,15 +5,20 @@ using System.Text;
 
 namespace MGB_one
 {
+    //Used by MGBGameParam class
+    //Type must be accesible in MainView - used for selection
     public enum MiniGameType { None = 0, Lumbermill = 1, Quarry = 2, ShootingRange = 3, Pond = 4 }
 }
 
 namespace MGB_one.Model.MGBAccounts
-{    
-
+{
+    //Contains minigame type and points left for level of minigame
+    //Used by MGBAccount class for each minigame level in an array
     public class MGBGameParam
     {
+        //Selected type of mini game - default "None"
         protected MiniGameType _gameType;
+        //Points left for this level of minigame - value range 0 - const Settings.MGB_MAX_GAME_POINTS
         protected byte _lvlPointsLeft;
 
         public MiniGameType GameType
