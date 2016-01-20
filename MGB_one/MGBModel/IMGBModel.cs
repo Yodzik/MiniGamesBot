25521c22fe41adbc10e408269f8369c9d0f28e0e
Ja <misiek777a@interia.pl>
2016-01-20 02:35:41 +0100

    added chosen accounts properties, events, methods

--- MGB_one/MGBModel/IMGBModel.cs
+++ MGB_one/MGBModel/IMGBModel.cs
@@ -9,12 +9,15 @@ namespace MGB_one.Model
     public interface IMGBModel
     {
         event EventHandler BaseAccountListChanged;
+        event EventHandler ChosenAccountListChanged;
         event EventHandler<ErrorEventArgs> ErrorOcured;
 
         int BaseAccountListCount { get; }
+        int ChosenAccountListCount { get; }
 
         //Contains state information vor view about AccountBase objects:
         List<string> BaseAccountsToStringListState();
+        List<string> ChosenAccountsToStringListState();
         //Contains base information for view about AccountBase objects:
         List<string> BaseAccountsToStringListBase(); 
         void BaseAccountListSaveToFile(string filePath);
@@ -22,6 +25,10 @@ namespace MGB_one.Model
         MGBAccountBase GetBaseAccountAt(int index);
         bool SetBaseAccountAt(string name, string login, string password, byte position, int index);
         bool RemoveBaseAccountAt(int index);
-        bool AddBaseAccount(string name, string login, string password, byte position);        
+        bool AddBaseAccount(string name, string login, string password, byte position);
+        void AddChosenAccount(int index);
+        void RemoveChosenAccount(int index);
+        MGBAccount GetChosenAccountAt(int index);
+        bool SetChosenAccountAt(MiniGameType gameType, byte[] gameLvlPoints, int index);
     }
 }
