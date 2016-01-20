c4e2eda9026dd5e08ee70ff16a631f83ec186e4d
Ja <misiek777a@interia.pl>
2016-01-20 02:36:19 +0100

    added chosen accounts properties, events, methods

--- MGB_one/MGBModel/MGBModel.cs
+++ MGB_one/MGBModel/MGBModel.cs
@@ -11,18 +11,26 @@ namespace MGB_one.Model
     public class MGBModel : IMGBModel
     {
         private MGBAccountBaseList _baseAccountList;
+        private List<MGBAccount> _chosenAccountList;
 
         public int BaseAccountListCount
         {
             get { return this._baseAccountList.Count(); }
         }
 
+        public int ChosenAccountListCount
+        {
+            get { return this._chosenAccountList.Count(); }
+        }
+
         public event EventHandler BaseAccountListChanged;
+        public event EventHandler ChosenAccountListChanged;
         public event EventHandler<ErrorEventArgs> ErrorOcured;
 
         public MGBModel()
         {
             this._baseAccountList = new MGBAccountBaseList();
+            this._chosenAccountList = new List<MGBAccount>();
             //tu eventy chyba bez sensu - subskrybcja po wywołaniu konstruktora
             if (!this._baseAccountList.LoadFromXmlFile(Settings.BaseAccountsFilePath))
             {
@@ -35,6 +43,16 @@ namespace MGB_one.Model
         public List<string> BaseAccountsToStringListState()
         {
             return this._baseAccountList.ToStringListState();
+        }
+
+        public List<string> ChosenAccountsToStringListState()
+        {
+            List<string> tempList = new List<string>();
+            foreach (MGBAccount account in this._chosenAccountList)
+            {
+                tempList.Add(account.AccountString());
+            }
+            return tempList;
         }
 
         public List<string> BaseAccountsToStringListBase()
@@ -122,11 +140,79 @@ namespace MGB_one.Model
             }
         }
 
+        public void AddChosenAccount(int index)
+        {
+            if (index >= 0 && index < this._baseAccountList.Count)
+            {
+                bool exists = false;
+                foreach (MGBAccount account in this._chosenAccountList)
+                {
+                    if (account.AccountBase.Equals(this._baseAccountList[index]))
+                        exists = true;
+                }
+                if (!exists)
+                    this._chosenAccountList.Add(new MGBAccount(this._baseAccountList[index]));
+            }
+            else
+                OnErrorOcured(this, new ErrorEventArgs(ErrorMessages.IndexOutOfBounds, ErrorSources.ModelAccountLists));
+        }
+
+        public void RemoveChosenAccount(int index)
+        {
+            if (index >= 0 && index < this._chosenAccountList.Count)
+                this._chosenAccountList.RemoveAt(index);
+            else
+                OnErrorOcured(this, new ErrorEventArgs(ErrorMessages.IndexOutOfBounds, ErrorSources.ModelChosenAccountList));
+        }
+
+        public MGBAccount GetChosenAccountAt(int index)
+        {
+            if (index >= 0 && index < this._chosenAccountList.Count)
+                return this._chosenAccountList[index];
+            else
+            {
+                OnErrorOcured(this, new ErrorEventArgs(ErrorMessages.IndexOutOfBounds, ErrorSources.ModelChosenAccountList));
+                return null;
+            }
+        }
+        public bool SetChosenAccountAt(MiniGameType gameType, byte[] gameLvlPoints, int index)
+        {
+            if (index >= 0 && index < this._chosenAccountList.Count)
+            {
+                if (gameLvlPoints.Length == Settings.MGB_MAX_GAME_LVL)
+                {
+                    for (int i = 0; i < gameLvlPoints.Length; i++)
+                    {
+                        this._chosenAccountList[index].SetGame(i, gameType, gameLvlPoints[i]);
+                    }
+                    return true;
+                }
+                else
+                {
+                    OnErrorOcured(this, new ErrorEventArgs(ErrorMessages.InvalidTableSize, ErrorSources.ModelChosenAccountList));
+                    return false;
+                }
+            }
+            else
+            {
+                OnErrorOcured(this, new ErrorEventArgs(ErrorMessages.IndexOutOfBounds, ErrorSources.ModelChosenAccountList));
+                return false;
+            }
+        }
+
         protected virtual void OnBaseAccountListChanged()
         {
             if (this.BaseAccountListChanged != null)
             {
                 BaseAccountListChanged(this, EventArgs.Empty);
+            }
+        }
+
+        protected virtual void OnChosenAccountListChanged()
+        {
+            if (this.ChosenAccountListChanged != null)
+            {
+                ChosenAccountListChanged(this, EventArgs.Empty);
             }
         }
 
