ea50aebdb6d7e712ea41c0ee55d3534f76c58fc3
Ja <misiek777a@interia.pl>
2016-01-22 05:38:18 +0100

    modified AccountString(), changed reward representation

--- MGB_one/MGBModel/MGBAccounts/MGBAccount.cs
+++ MGB_one/MGBModel/MGBAccounts/MGBAccount.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using MGB_one.Model;
+//using MGB_one.Model;
 
 namespace MGB_one.Model.MGBAccounts
 {
@@ -11,16 +11,14 @@ namespace MGB_one.Model.MGBAccounts
     {
         protected MGBAccountBase _accountBase;
         //array of parameters for each level of minigame
-        protected MGBGameParam[] _gamesLeft;
+        protected MGBRewardArray _gamesLeft;
         protected MiniGameType _gameType;
 
         public MGBAccountBase AccountBase { get { return this._accountBase; } }
-        public MGBGameParam[] GamesLeft
+        public MGBRewardArray GamesLeft
         {
-            get
-            {
-                return _gamesLeft;
-            }
+            get { return _gamesLeft; }
+            set { _gamesLeft = value; }
         }
         public MiniGameType GameType { get { return this._gameType; } }
         
@@ -28,11 +26,7 @@ namespace MGB_one.Model.MGBAccounts
         private MGBAccount()
         {
             this._accountBase = new MGBAccountBase();
-            this._gamesLeft = new MGBGameParam[Settings.MGB_MAX_GAME_LVL];
-            for (int i = 0; i<Settings.MGB_MAX_GAME_LVL; i++)
-            {
-                this._gamesLeft[i] = new MGBGameParam();
-            }
+            this._gamesLeft = new MGBRewardArray();
             this._gameType = MiniGameType.None;
         }
 
@@ -40,29 +34,6 @@ namespace MGB_one.Model.MGBAccounts
         {
             this._accountBase = source;
         }        
-
-        //probably unnecessary
-        public bool ValidateGames() 
-        {
-            if (this.GamesSum() <= Settings.MGB_MAX_GAME_POINTS)
-                return true;
-            else
-                return false;
-        }
-
-        //probably unnecessary
-        //sum > 20 means try your best in minigame, if fail to play max -> get next available reward
-        public byte GamesSum()
-        {
-            byte sum = 0;
-            foreach (MGBGameParam gameParam in this._gamesLeft)
-            {
-                sum += gameParam.LvlPointsLeft;
-            }
-            if (sum > Settings.MGB_MAX_GAME_POINTS)
-                sum = Settings.MGB_MAX_GAME_POINTS;
-            return sum;
-        }
 
         //false if values out of range
         //if gametype = None -> values set to default
@@ -73,18 +44,12 @@ namespace MGB_one.Model.MGBAccounts
             {
                 if (gameType != MiniGameType.None)
                 {
-                    this._gamesLeft[gameLvl].GameType = gameType;
                     this._gameType = gameType;
-                    //unable to set more than 20 points to all games - commented code probably for disposal
-                    /* 
-                    if (gamePoints > Settings.MGB_MAX_GAME_POINTS - this.GamesSum())
-                        gamePoints = (byte)(Settings.MGB_MAX_GAME_POINTS - this.GamesSum());
-                    */
-                    this._gamesLeft[gameLvl].LvlPointsLeft = gamePoints;
+                    this._gamesLeft.SetValueAt(gameLvl - 1, gamePoints);
                 }
                 else
                 {
-                    this._gamesLeft[gameLvl].Reset();
+                    this._gamesLeft.ResetAt(gameLvl - 1);
                     this._gameType = MiniGameType.None;
                 }            
                 return true;
@@ -93,28 +58,16 @@ namespace MGB_one.Model.MGBAccounts
                 return false;
         }
 
-        //not sure if neccessary
-        //copy for security purposes in view -> no reference = no problem
-        public MGBGameParam[] ReturnCopyOfParam()
-        {
-            MGBGameParam[] gamesLeft = new MGBGameParam[Settings.MGB_MAX_GAME_LVL];
-            for (int i = 0; i < Settings.MGB_MAX_GAME_LVL; i++)
-            {
-                gamesLeft[i].GameType = this._gamesLeft[i].GameType;
-                gamesLeft[i].LvlPointsLeft = this._gamesLeft[i].LvlPointsLeft;
-            }
-            return gamesLeft;
-        }
-
+       
         public string AccountString()
         {
-            return this._accountBase.ID.ToString() + ". " + this._accountBase.Name + " P:" + 
-                this._accountBase.PointsLeft.ToString() + " " + this._gameType.ToString() + 
-                " [L5=" + this._gamesLeft[4].LvlPointsLeft.ToString() + 
-                ",L4=" + this._gamesLeft[3].LvlPointsLeft.ToString() +
-                ",L3=" + this._gamesLeft[2].LvlPointsLeft.ToString() +
-                ",L2=" + this._gamesLeft[1].LvlPointsLeft.ToString() +
-                ",L1=" + this._gamesLeft[0].LvlPointsLeft.ToString() + "]";
+            return this._accountBase.Name + " P:" + this._accountBase.PointsLeft.ToString() +
+                " " + this._gameType.ToString() + 
+                " [" + this._gamesLeft.Rewards[4].ToString() + 
+                "," + this._gamesLeft.Rewards[3].ToString() +
+                "," + this._gamesLeft.Rewards[2].ToString() +
+                "," + this._gamesLeft.Rewards[1].ToString() +
+                "," + this._gamesLeft.Rewards[0].ToString() + "]";
         }
     }
 }
