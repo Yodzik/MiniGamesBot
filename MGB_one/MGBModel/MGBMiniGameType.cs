26aff39ef7889ee3e1c6ec6631c6769fae7f68eb
Ja <misiek777a@interia.pl>
2016-01-22 05:43:07 +0100

    deleted all representation for game lvl rewards, only enum now

---
+++ MGB_one/MGBModel/MGBMiniGameType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MGB_one
+{
+    //Type must be accesible in MainView - used for selection
+    public enum MiniGameType { None = 0, Lumbermill = 1, Quarry = 2, ShootingRange = 3, Pond = 4 }
+}
